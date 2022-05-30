29d032741cd00fb7be3a448e242e00d265bb771a
TriDo <tria3ltt@gmail.com>
2022-05-30 17:25:37 +0700

    correct STRUCT type

--- serverC/code/room.h
+++ serverC/code/room.h
@@ -1,9 +1,14 @@
 
+typedef struct 
+{
+    char username[50];
+    int totalPoint; //initial is 0
+} UserPoint;
+
 typedef struct
 {
     char questionSuiteId[50];
-    char username[50];
-    int totalPoint; //initial is 0
+    UserPoint userAndPoint[100]; //temporary limitation to 100 users
 } Room;
 
 int assignNewUserToRoom(char *username, int initialPoint); //append to file txt
