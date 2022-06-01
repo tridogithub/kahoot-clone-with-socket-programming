7b36baae2c12f817a5d44277be418d6cbe8ff890
TriDo <tria3ltt@gmail.com>
2022-06-01 14:28:06 +0700

    Code structure for main file

---
+++ serverC/code/constant.h
@@ -0,0 +1,15 @@
+//CODE
+#define LOGIN "login" //receive from client
+#define REGISTER "reg" //receive from client
+#define REQUESTQS "requestQS" //receive from client
+#define GETQS "getQS"
+#define CREATEROOM "cr" //receive from client
+#define GETROOMCODE "getRoomCode" 
+#define JOINROOM "joinRoom" //receive from client
+#define REQUESTQ "reqQ" //receive from client
+#define GETQ "getQ"
+#define SENDANS "sendAns" //receive from client
+#define REQUESTPOINT "reqTotalPoint" //receive from client
+#define GETPOINT "getTotalPoint"
+#define REQUESTRANKTABLE "reqRankTable" //receive from client
+#define GETRANKTABLE "getRankTable"
