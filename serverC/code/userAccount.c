09cae1b5d8a3a99e43946617aa97ffc8f8a3db1f
ngasocute <nguyennga1606ictbk@gmail.com>
2022-06-14 22:01:51 +0700

    login

--- serverC/code/userAccount.c
+++ serverC/code/userAccount.c
@@ -4,7 +4,31 @@
 
 int authenticated = 0;
 char *currentUser;
-int checkAuthenticated(char *username, char *password); // check in file user-account.txt
+int checkAuthenticated(char *username, char *password){
+    printf("\n %s", username);
+    printf("\n %s", password);
+    FILE *fp = fopen("user-account.txt", "r");
+    char buf[1024];
+    while (fgets(buf, sizeof(buf), fp) !=NULL) {
+        char *token = strtok(buf, " ");
+        
+        if (strcmp(username, token) == 0)
+        {
+            token = strtok(NULL, " ");
+            token[strlen(token)-1] = '\0';
+            printf("\n%d", strcmp(password, token));
+            if (strcmp(password, token) == 0)
+            {
+                printf("\nAuthenticated");
+                fclose(fp);
+                return 1;
+            }
+        }
+    }
+
+    fclose(fp);
+    return 0;
+}; // check in file user-account.txt
 void login(char *username, char *password, char *result)
 {
     if (checkAuthenticated(username, password) == 1)
@@ -22,4 +46,9 @@ void login(char *username, char *password, char *result)
     }
 }
 User *getAllUser();
-int registryNewUser(User newUser); // append to file
+int registryNewUser(User newUser){
+    FILE *fp = fopen("user-account.txt", "a");
+    fprintf(fp, "%s %s\n", newUser.username, newUser.password);
+    fclose(fp);
+    return 1; //registry new user
+}; // append to file
--- serverC/code/userAccount.h
+++ serverC/code/userAccount.h
@@ -5,35 +5,6 @@ typedef struct
     char password[50];
 } User;
 
-int checkAuthenticated(char *username, char *password){
-    printf("\n %s", username);
-    printf("\n %s", password);
-    FILE *fp = fopen("user-account.txt", "r");
-    char buf[1024];
-    while (fgets(buf, sizeof(buf), fp) !=NULL) {
-        char *token = strtok(buf, " ");
-        
-        if (strcmp(username, token) == 0)
-        {
-            token = strtok(NULL, " ");
-            token[strlen(token)-1] = '\0';
-            printf("\n%d", strcmp(password, token));
-            if (strcmp(password, token) == 0)
-            {
-                printf("\nAuthenticated");
-                fclose(fp);
-                return 1;
-            }
-        }
-    }
-
-    fclose(fp);
-    return 0;
-} //check in file user-account.txt
+int checkAuthenticated(char *username, char *password); //check in file user-account.txt
 User *getAllUser();
-int registryNewUser(User newUser){
-    FILE *fp = fopen("user-account.txt", "a");
-    fprintf(fp, "%s %s\n", newUser.username, newUser.password);
-    fclose(fp);
-    return 1; //registry new user
-}; //append to file
+int registryNewUser(User newUser); //append to file
