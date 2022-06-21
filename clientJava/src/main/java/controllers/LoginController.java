f81b835e7db49cc4c1788284340bac6f49e39b88
TriDo <tria3ltt@gmail.com>
2022-06-22 00:14:34 +0700

    Add client interface code

---
+++ clientJava/src/main/java/controllers/LoginController.java
@@ -0,0 +1,103 @@
+package controllers;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+/**
+ * @author oXCToo
+ */
+public class LoginController implements Initializable {
+
+    @FXML
+    private Label lblErrors;
+
+    @FXML
+    private TextField txtUsername;
+
+    @FXML
+    private TextField txtPassword;
+
+    @FXML
+    private Button btnSignin;
+
+    @FXML
+    public void handleButtonAction(MouseEvent event) {
+
+        if (event.getSource() == btnSignin) {
+            //login here
+            if (logIn().equals("Success")) {
+                try {
+
+                    //add you loading or delays - ;-)
+                    Node node = (Node) event.getSource();
+                    Stage stage = (Stage) node.getScene().getWindow();
+                    //stage.setMaximized(true);
+                    stage.close();
+                    Scene scene = new Scene(FXMLLoader.load(getClass().getResource("/fxml/CreateRoom.fxml")));
+                    stage.setScene(scene);
+                    stage.show();
+
+                } catch (IOException ex) {
+                    System.err.println(ex.getMessage());
+                }
+
+            }
+        }
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+//        if (con == null) {
+//            lblErrors.setTextFill(Color.TOMATO);
+//            lblErrors.setText("Server Error : Check");
+//        } else {
+//            lblErrors.setTextFill(Color.GREEN);
+//            lblErrors.setText("Server is up : Good to go");
+//        }
+    }
+
+    public LoginController() {
+
+    }
+
+    //we gonna use string to check for status
+    private String logIn() {
+        String status = "Success";
+        String email = txtUsername.getText();
+        String password = txtPassword.getText();
+        if (email.isEmpty() || password.isEmpty()) {
+            setLblError(Color.TOMATO, "Empty credentials");
+            status = "Error";
+        } else {
+            //TODO: send request to server socket
+        }
+
+        return status;
+    }
+
+    private void setLblError(Color color, String text) {
+        lblErrors.setTextFill(color);
+        lblErrors.setText(text);
+        System.out.println(text);
+    }
+}
